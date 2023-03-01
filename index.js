c6377cb54e350fdbd266a528cf9eaea98bd62fa9
ktadique <117470787+ktadique@users.noreply.github.com>
2023-03-01 22:31:11 +0000

    added manager prompts

--- index.js
+++ index.js
@@ -11,3 +11,40 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+
+const employeeTeam = [];
+
+function initManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter the manager's name:",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "Please enter the manager's ID number:",
+        name: "managerId",
+      },
+      {
+        type: "input",
+        message: "Please enter the manager's email:",
+        name: "managerEmail",
+      },
+      {
+        type: "input",
+        message: "Please enter the manager's office number:",
+        name: "managerOffice",
+      },
+    ])
+    .then((response) => {
+      const manager = new Manager(
+        response.managerName,
+        response.managerId,
+        response.managerEmail,
+        response.managerOffice
+      );
+      employeeTeam.push(manager);
+    });
+}
